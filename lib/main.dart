4954f38cddb27337da44f3a3f15f5656eca9834c
Emyboy <emyboybeats@gmail.com>
2020-06-12 14:40:06 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      appBar: AppBar(
+        title: Text('List App'),
+      ),
+      body: Column(
+        children: <Widget>[Text('Test')],
+      ),
+    );
+  }
+}
